db96622cbd2288c47f5121b711132101bfcfa60a
DrazonP <114837803+DrazonP@users.noreply.github.com>
2023-08-26 22:44:37 -0500

    Update item_constants.asm
    added icebeam thunderbolt and flamethrower to tms

--- constants/item_constants.asm
+++ constants/item_constants.asm
@@ -269,6 +269,11 @@ DEF TM01 EQU const_value
 	add_tm FIRE_PUNCH   ; f0
 	add_tm FURY_CUTTER  ; f1
 	add_tm NIGHTMARE    ; f2
+  add_tm FLAMETHROWER
+  add_tm THUNDERBOLT
+  add_tm ICE_BEAM
+  add_tm
+  add_tm
 DEF NUM_TMS EQU __tmhm_value__ - 1
 
 MACRO add_hm
